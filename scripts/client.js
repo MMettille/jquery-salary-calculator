2baa642789f274460c8eafd4ffa9bc0bd42a27c0
Mary Mettille <mary.mettille.brist@gmail.com>
2021-05-14 17:07:07 -0500

    created employee object

--- scripts/client.js
+++ scripts/client.js
@@ -3,5 +3,20 @@ $( document ).ready(readyNow);
 function readyNow(){
     // jQuery and JS is loaded, okay to manipulate the DOM
     console.log( 'in readyNow function' );
-
+    // What happens when the submit button is clicked
+    $( '#submitBtn' ).on('click', getUserInputs)
 }
+
+let employeeArray = [];
+
+function getUserInputs(){
+    // Checking to see if function is being called
+    console.log( 'in getUserInputs function' );
+    // targeting user's inputs with the DOM and creating an object
+    let employee = {
+        firstName: $( '#firstNameInput').val(),
+        lastName: $( '#lastNameInput' ).val(),
+        id: $( '#idInput' ).val(),
+        role: $( 'roleInput').val(),
+        salary:  $( 'salaryInput' ).val()
+    } // end employee
